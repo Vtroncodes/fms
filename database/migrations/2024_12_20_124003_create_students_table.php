4c9b13d732bff8606de88960aa3582aaf3a4f1c9
punitvardaam <team@vardaam.com>
2024-12-20 19:12:53 +0530

    Initial commit

---
+++ database/migrations/2024_12_20_124003_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignid('class_id')->constrained('Classes');
+            $table->foreignid('section_id')->constrained();
+            $table->string('name');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
